07bd9f94a6e7610cf3eed198b4387888014a9635
Radmir <QTU100@gmail.com>
2019-10-08 12:35:50 +0500

    Add project files.

---
+++ MVVMUtilities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMUtilities
+{
+    public class Class1
+    {
+    }
+}
